ca9f91e47a259b64205d93ee26e1fd987304d8b8
Kenneth Donahue <verykenny@gmail.com>
2016-03-12 16:00:19 -0700

    Changes from lesson 1

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -1,3 +1,89 @@
-/*
-This is empty on purpose! Your code to build the resume will go here.
- */
+
+var bio = {
+	"name" : "Kenneth Donahue",
+	"role" : "Web Developer",
+	"contacts" : {
+		"email" : "verykenny@gmail.com",
+		"mobile" : "720-468-8648",
+		"github" : "verykenny",
+		"twitter" : "verykenny",
+		"location" : "Boulder, CO"
+	},
+	"picutreUrl" : "images/fry.jpg ",
+	"welcomeMessage" : "Hi there!",
+	"skills" : [
+		"awesomeness", "programming"
+	]
+};
+
+var work = {
+	"jobs" : [{
+		"title" : "Knowledge Management",
+		"employer" : "IBM",
+		"years" : 3,
+		"location" : "Boulder, CO",
+		"description" : " "
+	}, {
+		"title" : "Administrative Assistant",
+		"employer" : "Graduate School of Banking at Colorado",
+		"years" : .2,
+		"location" : "Boulder, CO",
+		"description" : " "
+	}]
+};
+
+var projects = {
+	"projects" : [{
+		"title" : "Portfolio",
+		"dates" : "Feb, 2016",
+		"description" : "Porfolio project for Front End Web Development",
+		"image" : "images/197x148.gif"
+	}, {
+		"title" : "Resume",
+		"dates" : "Mar, 2016",
+		"description" : "Resume project for Front End Web Development",
+		"image" : "images/197x148.gif"
+	}]
+}
+
+var education = {
+	"schools" : [{
+		"name" : "University of Colorado at Boulder",
+		"location" : "Boulder, CO",
+		"degree" : "BM",
+		"major" : "Music Performance, Vocal",
+		"dates" : "09 - 12"
+	}, {	
+		"name" : "Northern State University",
+		"location" : "Aberdeen, SD",
+		"degree" : "BA",
+		"major" : "Music",
+		"dates" : "07 - 09"
+	}],
+	"online courses" : [{
+		"school" : "Udacity",
+		"title" : "JavaScript Basics",
+		"dates" : "Mar, 2016",
+		"url" : "https://www.udacity.com/course/progress#!/c-ud804-nd"
+	}, {
+		"school" : "Udacity",
+		"title" : "Responsive Images",
+		"dates" : "Mar, 2016",
+		"url" : "https://www.udacity.com/course/progress#!/c-ud882-nd"
+	}, {
+		"school" : "Udacity",
+		"title" : "Responsive Web Design Fundamentals",
+		"dates" : "Mar, 2016",
+		"url" : "https://www.udacity.com/course/progress#!/c-ud893-nd"
+	}, {
+		"school" : "Udacity",
+		"title" : "Intro to HTML and CSS",
+		"dates" : "Mar, 2016",
+		"url" : "https://www.udacity.com/course/progress#!/c-ud304-nd"
+	}, {
+		"school" : "Udacity",
+		"title" : "How to Use Git and GitHub",
+		"dates" : "Mar, 2016",
+		"url" : "https://www.udacity.com/course/progress#!/c-ud775-nd"
+	}]	
+}
